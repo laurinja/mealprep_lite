6fd6f49889a6c084abc6109db08879df14da10e8
VctPaes <laurabareto@alunos.utfpr.edu.br>
2025-11-30 18:08:22 -0300

    Add ingredient and tag entities, DTOs, and mappers
    Introduces core files for ingredients and tags, including DTOs, entities, and mappers. Updates meal local datasource to support new tag and ingredient IDs in RefeicaoDTO. These changes lay the groundwork for handling ingredients and tags in the domain and data layers.

--- lib/features/meal/data/datasources/meal_local_datasource.dart
+++ lib/features/meal/data/datasources/meal_local_datasource.dart
@@ -10,13 +10,55 @@ class MealLocalDataSourceImpl implements MealLocalDataSource {
     await Future.delayed(const Duration(milliseconds: 300));
 
     return [
-      RefeicaoDTO(id: '1', nome: 'Aveia com Frutas', tipo: 'Café da Manhã', tagIds: ['Rápido', 'Saudável']),
-      RefeicaoDTO(id: '2', nome: 'Ovos Mexidos', tipo: 'Café da Manhã', tagIds: ['Rápido']),
-      RefeicaoDTO(id: '3', nome: 'Salada de Grão de Bico', tipo: 'Almoço', tagIds: ['Saudável']),
-      RefeicaoDTO(id: '4', nome: 'Macarrão com Pesto', tipo: 'Almoço', tagIds: ['Rápido', 'Vegetariano']),
-      RefeicaoDTO(id: '5', nome: 'Wrap de Frango', tipo: 'Almoço', tagIds: ['Rápido']),
-      RefeicaoDTO(id: '6', nome: 'Sopa de Legumes', tipo: 'Jantar', tagIds: ['Saudável', 'Vegetariano']),
-      RefeicaoDTO(id: '7', nome: 'Peixe Grelhado', tipo: 'Jantar', tagIds: ['Saudável']),
+      RefeicaoDTO(
+        id: '1', 
+        nome: 'Aveia com Frutas', 
+        tipo: 'Café da Manhã', 
+        tag_ids: ['Rápido', 'Saudável'], 
+        ingrediente_ids: [], 
+      ),
+      RefeicaoDTO(
+        id: '2', 
+        nome: 'Ovos Mexidos', 
+        tipo: 'Café da Manhã', 
+        tag_ids: ['Rápido'], 
+        ingrediente_ids: [],
+      ),
+      RefeicaoDTO(
+        id: '3', 
+        nome: 'Salada de Grão de Bico', 
+        tipo: 'Almoço', 
+        tag_ids: ['Saudável'], 
+        ingrediente_ids: [],
+      ),
+      RefeicaoDTO(
+        id: '4', 
+        nome: 'Macarrão com Pesto', 
+        tipo: 'Almoço', 
+        tag_ids: ['Rápido', 'Vegetariano'], 
+        ingrediente_ids: [],
+      ),
+      RefeicaoDTO(
+        id: '5', 
+        nome: 'Wrap de Frango', 
+        tipo: 'Almoço', 
+        tag_ids: ['Rápido'], 
+        ingrediente_ids: [],
+      ),
+      RefeicaoDTO(
+        id: '6', 
+        nome: 'Sopa de Legumes', 
+        tipo: 'Jantar', 
+        tag_ids: ['Saudável', 'Vegetariano'], 
+        ingrediente_ids: [],
+      ),
+      RefeicaoDTO(
+        id: '7', 
+        nome: 'Peixe Grelhado', 
+        tipo: 'Jantar', 
+        tag_ids: ['Saudável'], 
+        ingrediente_ids: [],
+      ),
     ];
   }
 }
